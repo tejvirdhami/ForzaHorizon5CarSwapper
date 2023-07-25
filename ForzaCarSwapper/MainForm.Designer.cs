942df91f813331e3d120a3538f549d0e8f6bf604
Tejvir Dhami <62151509+tejvirdhami@users.noreply.github.com>
2023-07-24 21:47:22 -0400

    Add project files.

---
+++ ForzaCarSwapper/MainForm.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace ForzaCarSwapper
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.BGWorker = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ProcOpenLabel = new System.Windows.Forms.Label();
+            this.btnScan = new System.Windows.Forms.Button();
+            this.lblScanStatus = new System.Windows.Forms.Label();
+            this.btnSwap = new System.Windows.Forms.Button();
+            this.txtSwapId = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BGWorker
+            // 
+            this.BGWorker.WorkerReportsProgress = true;
+            this.BGWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BGWorker_DoWork);
+            this.BGWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.BGWorker_ProgressChanged);
+            this.BGWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BGWorker_RunWorkerCompleted);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(297, 138);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Process:";
+            // 
+            // ProcOpenLabel
+            // 
+            this.ProcOpenLabel.AutoSize = true;
+            this.ProcOpenLabel.Location = new System.Drawing.Point(372, 139);
+            this.ProcOpenLabel.Name = "ProcOpenLabel";
+            this.ProcOpenLabel.Size = new System.Drawing.Size(35, 20);
+            this.ProcOpenLabel.TabIndex = 1;
+            this.ProcOpenLabel.Text = "N/A";
+            // 
+            // btnScan
+            // 
+            this.btnScan.Location = new System.Drawing.Point(132, 70);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(148, 41);
+            this.btnScan.TabIndex = 2;
+            this.btnScan.Text = "Scan";
+            this.btnScan.UseVisualStyleBackColor = true;
+            this.btnScan.Click += new System.EventHandler(this.btnScan_Click);
+            // 
+            // lblScanStatus
+            // 
+            this.lblScanStatus.AutoSize = true;
+            this.lblScanStatus.Location = new System.Drawing.Point(76, 138);
+            this.lblScanStatus.Name = "lblScanStatus";
+            this.lblScanStatus.Size = new System.Drawing.Size(35, 20);
+            this.lblScanStatus.TabIndex = 3;
+            this.lblScanStatus.Text = "N/A";
+            // 
+            // btnSwap
+            // 
+            this.btnSwap.Location = new System.Drawing.Point(301, 70);
+            this.btnSwap.Name = "btnSwap";
+            this.btnSwap.Size = new System.Drawing.Size(148, 41);
+            this.btnSwap.TabIndex = 4;
+            this.btnSwap.Text = "Swap";
+            this.btnSwap.UseVisualStyleBackColor = true;
+            this.btnSwap.Click += new System.EventHandler(this.btnSwap_Click);
+            // 
+            // txtSwapId
+            // 
+            this.txtSwapId.Location = new System.Drawing.Point(132, 22);
+            this.txtSwapId.Name = "txtSwapId";
+            this.txtSwapId.Size = new System.Drawing.Size(317, 26);
+            this.txtSwapId.TabIndex = 5;
+            this.txtSwapId.TextChanged += new System.EventHandler(this.txtSwapId_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(10, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Status:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(10, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(116, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Swapping ID:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(128, 189);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(150, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "© 2023 Tejvir Dhami";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 218);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtSwapId);
+            this.Controls.Add(this.btnSwap);
+            this.Controls.Add(this.lblScanStatus);
+            this.Controls.Add(this.btnScan);
+            this.Controls.Add(this.ProcOpenLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Forza Car Swapper x64";
+            this.Shown += new System.EventHandler(this.MainForm_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker BGWorker;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label ProcOpenLabel;
+        private System.Windows.Forms.Button btnScan;
+        private System.Windows.Forms.Label lblScanStatus;
+        private System.Windows.Forms.Button btnSwap;
+        private System.Windows.Forms.TextBox txtSwapId;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ForzaCarSwapper/MainForm.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Memory;
+
+namespace ForzaCarSwapper
+{
+    public partial class MainForm : Form
+    {
+        public Mem m = new Mem();
+        private IEnumerable<long> Addresses = null;
+        public MainForm()
+        {
+            InitializeComponent();
+            btnSwap.Enabled = false;
+            txtSwapId.Enabled = false;
+        }
+
+        bool ProcOpen = false;
+
+        private void BGWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            ProcOpen = m.OpenProcess("ForzaHorizon5.exe");
+   
+                if(!ProcOpen)
+                    {
+                    Thread.Sleep(1000);
+                    return;
+                }
+
+                Thread.Sleep(1000);
+                BGWorker.ReportProgress(0);
+   
+        }
+
+        private void updateScanLabel(string message)
+        {
+            if (lblScanStatus.InvokeRequired)
+            {
+                lblScanStatus.BeginInvoke((MethodInvoker)(() => updateScanLabel(message)));
+            }
+            else
+            {
+                lblScanStatus.Text = message;
+            }
+        }
+
+        private void MainForm_Shown(object sender, EventArgs e)
+        {
+            BGWorker.RunWorkerAsync();
+        }
+
+        private void BGWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if(ProcOpen)
+            {
+                ProcOpenLabel.Text = "Game Found";
+                ProcOpenLabel.ForeColor = Color.Green;
+            }
+        }
+
+        private void BGWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            BGWorker.RunWorkerAsync();
+        }
+
+        private void btnScan_Click(object sender, EventArgs e)
+        {
+            updateScanLabel("");
+            Addresses = null;
+
+            Thread ScanThread = new Thread(async () =>
+            {
+                try
+                {
+                    updateScanLabel("Scanning...");
+                    Addresses = await m.AoBScan("BB 0B 00 00 00 00 00 00 02 00 00 00 00 00 00 00 00", true, true, false);
+                }
+                catch { }
+
+                if (Addresses != null)
+                {
+                    if (btnSwap.InvokeRequired)
+                    {
+                        btnSwap.BeginInvoke((MethodInvoker)(() => {
+                            updateScanLabel("Viper found!");
+                            lblScanStatus.ForeColor = Color.Green;
+                            btnSwap.Enabled = true;
+                            txtSwapId.Enabled = true;
+                        }));
+                    }
+                    else
+                    {
+                        updateScanLabel("Viper found!");
+                        lblScanStatus.ForeColor = Color.Green;
+                        btnSwap.Enabled = true;
+                        txtSwapId.Enabled = true;
+                    }
+                }
+                else
+                    updateScanLabel("Failed to find Viper!");
+                lblScanStatus.ForeColor = Color.Red;
+            });
+            ScanThread.Start();
+        }
+
+        private void btnSwap_Click(object sender, EventArgs e)
+        {
+            if (Regex.IsMatch(txtSwapId.Text, @"^[0-9]+$") && Addresses != null)
+            {
+                foreach (long addr in Addresses)
+                    m.WriteMemory(addr.ToString("X"), "2bytes", txtSwapId.Text);
+
+                lblScanStatus.ForeColor = Color.Green;
+                updateScanLabel("Swap Successful!");
+            }
+            else if (Addresses == null)
+            {
+                lblScanStatus.ForeColor = Color.Red;
+                updateScanLabel("Swap Failed!");
+                MessageBox.Show("Scan Needed", "Alert", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else
+                MessageBox.Show("Numbers Only", "Alert", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        private void txtSwapId_TextChanged(object sender, EventArgs e)
+        {
+            updateScanLabel("N/A");
+            lblScanStatus.ForeColor= Color.Black;
+        }
+    }
+}
